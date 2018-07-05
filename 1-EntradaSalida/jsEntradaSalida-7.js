c19211feb156f18ba406905b95bef114bd2cb0b3
victoriakamelia <victoriakamelia@gmail.com>
2018-07-05 09:20:13 -0300

    ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,58 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+    numeroUno=document.getElementById("numeroUno").value;
+    numeroDos=document.getElementById("numeroDos").value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    resultado=numeroUno+numeroDos;
+    alert("La respuesta es "+resultado);
+    
 	
 }
 
 function restar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+    numeroUno=document.getElementById("numeroUno").value;
+    numeroDos=document.getElementById("numeroDos").value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    resultado=numeroUno-numeroDos;
+    alert("La respuesta es "+resultado);
 	
 }
 
 function multiplicar()
 { 
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+    numeroUno=document.getElementById("numeroUno").value;
+    numeroDos=document.getElementById("numeroDos").value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    resultado=numeroUno*numeroDos;
+    alert("La respuesta es "+resultado);
 	
 }
 
 function dividir()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+    numeroUno=document.getElementById("numeroUno").value;
+    numeroDos=document.getElementById("numeroDos").value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    resultado=numeroUno/numeroDos;
+    alert("La respuesta es "+resultado);
 	
 }
 
