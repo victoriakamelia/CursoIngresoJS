33478ddf54a2edfc074d54e59eebde0d351ee817
victoriakamelia <victoriakamelia@gmail.com>
2018-07-05 11:16:01 -0300

    tp1

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,51 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var resultado;
+    precioUno=document.getElementById("PrecioUno").value;
+    precioDos=document.getElementById("PrecioDos").value;
+    precioTres=document.getElementById("PrecioTres").value;
+    precioUno=parseInt(precioUno);
+    precioDos=parseInt(precioDos);
+    precioTres=parseInt(precioTres);
+    resultado=precioUno+precioDos+precioTres;
+    alert(resultado);
+  
 	
 }
 function Promedio () 
 {
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var resultado;
+    precioUno=document.getElementById("PrecioUno").value;
+    precioDos=document.getElementById("PrecioDos").value;
+    precioTres=document.getElementById("PrecioTres").value;
+    precioUno=parseInt(precioUno);
+    precioDos=parseInt(precioDos);
+    precioTres=parseInt(precioTres);
+    resultado=(precioUno+precioDos+precioTres)/3;
+    alert(resultado);
+
 	
 }
 function PrecioFinal () 
 {
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var resultado;
+    precioUno=document.getElementById("PrecioUno").value;
+    precioDos=document.getElementById("PrecioDos").value;
+    precioTres=document.getElementById("PrecioTres").value;
+    precioUno=parseInt(precioUno);
+    precioDos=parseInt(precioDos);
+    precioTres=parseInt(precioTres);
+    resultado=(precioUno+precioDos+precioTres)*0.21+(precioUno+precioDos+precioTres);
+    alert(resultado);
 	
 }
