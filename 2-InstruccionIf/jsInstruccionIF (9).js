a3938d8ddd2e253da84417f3ceca9f6205c0eab8
victoriakamelia <victoriakamelia@gmail.com>
2018-07-10 12:23:32 -0300

    random

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	//Genero el número RANDOM entre 1 y 10 ç
+	var random;
+	random=Math.floor(Math.random()*(10-1)+1);
+	random=parseInt(random);
+	alert(random);
+
 	
 
 }//FIN DE LA FUNCIÓN
