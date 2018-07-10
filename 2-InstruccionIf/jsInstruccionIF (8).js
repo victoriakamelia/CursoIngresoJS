6bc257ec77a129fe14c061be123f2c7cbf7cd3c6
victoriakamelia <victoriakamelia@gmail.com>
2018-07-10 12:04:46 -0300

    correccion

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -5,7 +5,7 @@ var edad;
 var civil;
 civil=document.getElementById("estadoCivil").value;
 edad=document.getElementById("edad").value;
-if (edad > 18 && civil == "Soltero"){
+if (edad > 17 && civil == "Soltero"){
     alert("Usted es mayor de edad y es soltero.");
 }
 
