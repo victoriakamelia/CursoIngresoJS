4e67d3af0580d95475396d5dfb633f639a958910
victoriakamelia <victoriakamelia@gmail.com>
2018-07-11 23:59:35 -0300

    cas

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var random;
+	random=Math.floor(Math.random()*10+1);
+	random=parseInt(random);
+	if (random > = 9)
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,7 +2,7 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 ç
 	var random;
-	random=Math.floor(Math.random()*(10-1)+1);
+	random=Math.floor(Math.random()*(10)+1);
 	random=parseInt(random);
 	alert(random);
 
