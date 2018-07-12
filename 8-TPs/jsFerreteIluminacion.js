ddd95f6fb3b89555d91e84891a174d2bb54ab73b
victoriakamelia <victoriakamelia@gmail.com>
2018-07-12 11:13:09 -0300

    Signed-off-by: victoriakamelia <victoriakamelia@gmail.com>

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,47 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
+    var cantidadLamparitas;
+    var precio;
+    var descuento;
+    var precioDescuento;
+    var msg;
+    var marca;
+    
+    cantidadLamparitas=document.getElementById("Cantidad").value;
+    marca=document.getElementById("Marca").value;
+    cantidadLamparitas=parseInt(cantidadLamparitas);
+    precio=35;
+    msg="El descuento de su compra es: ";
+   
+    if (cantidadLamparitas >= 6){
+        precioDescuento=(cantidadLamparitas*precio)*0.5;
+        alert(msg+precioDescuento);
+    }
+
+    if (marca=="ArgentinaLuz" && cantidadLamparitas == 5){
+        precioDescuento=(cantidadLamparitas*precio)*0.4;
+        alert(msg+precioDescuento);
+    }else
+    { if (marca!="ArgentinaLuz" && cantidadLamparitas == 5)
+     {
+            precioDescuento=(cantidadLamparitas*precio)*0.3;
+            alert(msg+precioDescuento);
+     }
+    }
+
+    if (cantidadLamparitas==4 && (marca=="FelipeLamparas" || marca=="ArgentinaLuz"))
+    {
+        precioDescuento=(cantidadLamparitas*precio)*25/100;
+        alert(msg+precioDescuento);
+    }else
+    { if (cantidadLamparitas==4 && (marca!="FelipeLamparas" || marca!="ArgentinaLuz"))
+     {
+        precioDescuento=(cantidadLamparitas*precio)*20/100;
+        alert(msg+precioDescuento);
+     }
+
+    }
+    
  	
 }
