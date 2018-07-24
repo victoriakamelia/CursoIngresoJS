3a2b63e6833c0ca9fe3970c33f246edddb247f9b
alumno <alumno@utnfra.com.ar>
2018-07-24 12:16:34 -0300

    jghj

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,14 +2,36 @@ function mostrar()
 {
 
 	var contador=0;
-	//declarar contadores y variables 
-	
+	var negativos=0;
+	var positivos;
+	var cantPositivo;
+	var cantNegativos;
+	var ceros;
+	var pares;
+	var impares;
+	var promedioP;
+	var promedioN;
+	var diferenciaPn;
 	var respuesta="si";
+	var num;
+
+	num=prompt("Ingrese un número:");
+	num=parseInt(num);
+	while(isNaN(num)){
+		
+		num=prompt("Ingrese un número:");
+		num=parseInt(num);
+	}
+	respuesta=prompt("¿Desea continuar?");
 
 	while(respuesta!="no")
 	{
+		if(num<0){
+			contador++
+			num=num+negativos;
+		}
 		
-	
+	document.write()
 	}
 
 
