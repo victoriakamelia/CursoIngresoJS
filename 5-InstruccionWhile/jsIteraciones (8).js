fa1493feceeacfdd273cd6e68556b141079b3bbb
alumno <alumno@utnfra.com.ar>
2018-07-24 11:52:58 -0300

    222222222

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,26 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero=0;
 	var respuesta='si';
+
+	numero=prompt("Ingrese un número positivo o negativo");
+	numero=parseInt(numero);
+	
+	while(respuesta == "si"){
+		contador++;
+		
+		if(numero >= 0){
+			positivo+=numero;
+		} else {
+			negativo*=numero;
+		}
+		respuesta=prompt(¿Desea continuar?);
+
+
+
+
+	}
 
 
 document.getElementById('suma').value=positivo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,32 @@
 function mostrar()
 {
-
-	var contador=0;
-	// declarar variables
-	
 	var respuesta='si';
-
+	var max;
+	var min;
+	var num;
+	var bandera;
+	bandera=true;
+	
 	while(respuesta!='no')
 	{
+		num=prompt("Ingrese número");
+		num=parseInt(num);
+		if (bandera){
+			bandera=false;
+			max=num;
+			min=num;
 		
-	
+		}
+		if (num > max){
+			max=num;
+		}
+		if (num < min){
+			min=num;
+		}
+	respuesta=prompt("¿Desea continuar?");
 	}
-
-
+document.getElementById("maximo").value=max;
+document.getElementById("minimo").value=min;
 
 
 }//FIN DE LA FUNCIÓN
