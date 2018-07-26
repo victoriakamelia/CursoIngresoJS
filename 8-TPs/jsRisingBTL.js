95e550e6bc660ce6038d05c4c97fb4d689082a7f
victoriakamelia <victoriakamelia@gmail.com>
2018-07-26 12:11:31 -0300

    AAAA

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,60 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+    var edad;
+    var sexo;
+    var estadoCivil;
+    var sueldo;
+    var numLeg;
+    var nacionalidad;
+    
+    edad=parseInt(edad);
+    estadoCivil=parseInt(estadoCivil);
+    sueldo=parseInt(sueldo);
+    numLeg=parseInt(numLeg);
+    
+
+    while(edad < "18" || edad > "90")
+    {
+    edad=prompt("Ingrese su edad (entre 18 y 90 años, inclusive).");
+    }
+    document.getElementById("Edad").value=edad;
+    
+    while(sexo != "F" || sexo != "M")
+    {
+    sexo=prompt("Ingrese su sexo (F para femenino, M para masculino).");
+    }
+    document.getElementById("Sexo").value=sexo;
+    
+    
+    
+    while(estadoCivil != "1" && estadoCivil != "2" && estadoCivil != "3" && estadoCivil != "4" )
+    {
+    estadoCivil=prompt("Ingrese su estado civil (1-soltero/a, 2-casado/a, 3- divorciado/a, 4- viudo/a");
+    }
+    document.getElementById("EstadoCivil").value=estadoCivil;
+    
+    while(sueldo < 8000)
+    {
+    sueldo=prompt("Sueldo bruto (no menor a $8.000).");
+    }
+    document.getElementById("Sueldo").value=sueldo;
+
+    while(numLeg < "1000" || numLeg >9999)
+    {
+    numLeg=prompt("Número de legajo, numérico de 4 cifras, sin ceros a la izquierda");
+    }
+    document.getElementById("Legajo").value=numLeg;
+    
+    while(nacionalidad !="A" || nacionalidad !="E" || nacionalidad != "N" )
+    {
+    nacionalidad=prompt("Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.a");
+    }
+    document.getElementById("Nacionalidad").value=nacionalidad;
+
+
+    
+
+   
  
 }
