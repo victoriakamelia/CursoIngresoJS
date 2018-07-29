e94d1c417ce9d79f93e989fed64a33cdc6c463e8
victoriakamelia <victoriakamelia@gmail.com>
2018-07-29 20:57:49 -0300

    fsdf

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,68 +9,65 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
-    var edad;
-    var sexo;
-    var estadoCivil;
-    var sueldo;
-    var numLeg;
-    var nacionalidad;
-    
-    edad=parseInt(edad);
-    estadoCivil=parseInt(estadoCivil);
-    sueldo=parseInt(sueldo);
-    numLeg=parseInt(numLeg);
-    
-    edad=prompt("Ingrese su edad (entre 18 y 90 años, inclusive).");
+ var edad;
+ var sexo;
+ var estadoCivil;
+ var sueldo;
+ var legajo;
+ var nacionalidad;
 
-    while(edad < "18" || edad > "90")
-    {
-    edad=prompt("Ingrese su edad (entre 18 y 90 años, inclusive).");
+ edad=prompt("Ingrese su edad.");
+ edad=parseInt(edad);
+  while(edad < 18 || edad > 90 )
+  {
+      edad=prompt("El número ingresado no es correcto, por favor, ingrese un número mayor 18 y menor a 90.");
+      break;
+  }
+
+  sexo=prompt("Ingrese su sexo.");
+
+  while(sexo != "F" || sexo != "M")
+  {
+      sexo=prompt("La letra ingresada no es correcta, indique F para femenino o M para masculino");
+      break;
+  }
+
+  estadoCivil=prompt("Indique su estado civil: 1, para soltero/a, 2 para casado/a, 3 para divorciado/a, 4 para viudo/a.");
+  estadoCivil=parseInt(estadoCivil);
+  while(estadoCivil != "1" || estadoCivil != "2" || estadoCivil != "3" || estadoCivil != "4")
+  {
+    estadoCivil=prompt("Los datos ingresados no son correctos. Por favor, indique su estado civil: 1, para soltero/a, 2 para casado/a, 3 para divorciado/a, 4 para viudo/a.");
     break;
-    }
-    
-    sexo=prompt("Ingrese su sexo (F para femenino, M para masculino).");
-    while(sexo != "F" || sexo != "M")
-    {
-    sexo=prompt("Ingrese su sexo (F para femenino, M para masculino).");
-    break;
-    }
-    
-    
-    
-    estadoCivil=prompt("Ingrese su estado civil (1-soltero/a, 2-casado/a, 3- divorciado/a, 4- viudo/a");
-    while(estadoCivil != "1" && estadoCivil != "2" && estadoCivil != "3" && estadoCivil != "4" )
-    {
-    estadoCivil=prompt("Ingrese su estado civil (1-soltero/a, 2-casado/a, 3- divorciado/a, 4- viudo/a");
-    break;
-    }
-    
-    sueldo=prompt("Sueldo bruto (no menor a $8.000).");
-    while(sueldo < 8000)
-    {
-    sueldo=prompt("Sueldo bruto (no menor a $8.000).");
-    break;
-    }
-    
-    numLeg=prompt("Número de legajo, numérico de 4 cifras, sin ceros a la izquierda");
-    while(numLeg < "1000" || numLeg >"9999")
-    {
-    numLeg=prompt("Número de legajo, numérico de 4 cifras, sin ceros a la izquierda");
-    break;
-    }
-    
-    nacionalidad=prompt("Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.a");
-    while(nacionalidad !="A" || nacionalidad !="E" || nacionalidad != "N" )
-    {
-    nacionalidad=prompt("Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.a");
-    break;    
-}
-    
-   document.getElementById("Edad").value=edad;
-   document.getElementById("Sexo").value=sexo;
-   document.getElementById("EstadoCivil").value=estadoCivil;
-   document.getElementById("Sueldo").value=sueldo;
-   document.getElementById("Legajo").value=numLeg;
-   document.getElementById("Nacionalidad").value=nacionalidad;
+  }
  
+  sueldo=prompt("Indique su sueldo bruto, no menor a $8.000");
+  sueldo=parseInt(sueldo);
+  while(sueldo < "8000")
+  {
+      sueldo=prompt("El valor ingresado no es correcto, por favor indique su sueldo bruto, no menor a $8.000");
+      break;
+  }
+
+  legajo=prompt("Ingrese el número de su legajo (numérico de cuatro cifras, sin ceros a la izquierda).")
+  legajo=parseInt(legajo);
+  while(legajo < "1000" || legajo > "9999")
+  {
+    legajo=prompt("Los datos ingresados no son válidos, por favor ingrese el número de su legajo (numérico de cuatro cifras, sin ceros a la izquierda).")
+    break;
+  }
+
+  nacionalidad=prompt("Ingrese su nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+  while(nacionalidad != "A" || nacionalidad != "E" || nacionalidad != "N")
+  {
+    nacionalidad=prompt("Los datos ingresados no son correctos. Por favor ingrese su nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+    break;
+  }
+
+  document.getElementById("Edad").value=edad;
+  document.getElementById("Sexo").value=sexo;
+  document.getElementById("EstadoCivil").value=estadoCivil;
+  document.getElementById("Sueldo").value=sueldo;
+  document.getElementById("Legajo").value=legajo;
+  document.getElementById("Nacionalidad").value=nacionalidad;
+
 }
