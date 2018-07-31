544e744cf3fb8e8281ccd9448ca581b737d068a2
victoriakamelia <victoriakamelia@gmail.com>
2018-07-31 12:10:18 -0300

    bfggb

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,11 @@
 function mostrar()
-{
+{ 
+    var cont;
+    var numero=0;
 
+     for(cont=0; numero < 10; cont++)
+      {
+          numero=numero+1;
+          alert(numero);
+      }
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,13 @@
 function mostrar()
 {
+    var cont;
+    var numero=11;
+
+     for(cont=0; numero > 0; cont--)
+      {
+          numero=numero-1;
+          alert(numero);
+      }
 
 
 }
